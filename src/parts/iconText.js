b2b9ff775ca5cb20475784c6311781ea32d38d76
5210411131 Septian Yogi Kurniawan <septianyogol@gmail.com>
2023-07-13 20:51:39 +0700

    create app

---
+++ src/parts/iconText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'elements/Button';
+
+export default function IconText() {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+        <span className="text-indigo">Stay</span><span className="text-gray-900">cation.</span>
+    </Button>
+  );
+}
