e508a46510557dd6731139034db06c6662569b99
5210411131 Septian Yogi Kurniawan <septianyogol@gmail.com>
2023-07-17 22:48:05 +0700

    Section Testimonial

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
 import Categories from 'parts/Categories';
+import Testimony from 'parts/Testimony';
 
 import landingPage from 'json/landingPage.json';
 export default class LandingPage extends Component {
@@ -26,6 +27,8 @@ export default class LandingPage extends Component {
       <Categories
         data={landingPage.categories}
       />
+      <Testimony data={landingPage.testimonial}/>
+
       </>
     );
   }
